76f1bc83d37df1f46b83b3fe0e5a70a81ae12e55
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2022-09-15 20:22:41 +0200

    Edit footer (#2)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,17 +17,15 @@ export function Footer() {
   return (
     <footer className="mt-32">
       <Container.Outer>
-        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
+        <div className="pt-10 pb-16 border-t border-zinc-100 dark:border-zinc-700/40">
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/posts">Posts</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Spencer Sharp. All rights
+                &copy; {new Date().getFullYear()} Mirha Masala. All rights
                 reserved.
               </p>
             </div>
