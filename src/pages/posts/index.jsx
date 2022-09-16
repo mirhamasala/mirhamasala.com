fd5274119c0be976ba4a85921588c770a137c246
Mirha Masala <mirha@hey.com>
2022-09-16 09:43:32 +0200

    Capitalize title

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -36,7 +36,7 @@ export default function postsIndex({ posts }) {
   return (
     <>
       <Head>
-        <title>posts - Spencer Sharp</title>
+        <title>Posts - Spencer Sharp</title>
         <meta
           name="description"
           content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
