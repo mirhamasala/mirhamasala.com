07392a867ffe8d14ffe3d2be3d26e15cc32d526f
Mirha Masala <mirha@hey.com>
2022-09-16 09:53:51 +0200

    Add feed description

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -14,7 +14,7 @@ export async function generateRssFeed() {
 
   let feed = new Feed({
     title: author.name,
-    description: 'Your blog description',
+    description: 'A creative journal',
     author,
     id: siteUrl,
     link: siteUrl,
