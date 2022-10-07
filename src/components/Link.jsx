4b040d3652473b3a11cde103d6eacdcaa4a89aa3
Mirha Masala <mirha@hey.com>
2022-10-07 11:49:24 +0100

    Include footnotes in Link component

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,15 +1,20 @@
 import NextLink from 'next/link'
 
-export function Link({ href, children }) {
-  const isAnchor = href.startsWith('#')
+export function Link({ href, children, ...props }) {
+  const isFootNote = href.startsWith('#user-content')
+  const isAnchor = href.startsWith('#') && !isFootNote
   const isInternal = href.startsWith('/')
 
-  if (isAnchor) {
+  if (isFootNote) {
     return (
-      <NextLink href={href} scroll={false}>
+      <NextLink href={href} {...props}>
         {children}
       </NextLink>
     )
+  }
+
+  if (isAnchor) {
+    return <NextLink id={href.substring(1)} href={href} scroll={false} />
   }
 
   if (isInternal) {
