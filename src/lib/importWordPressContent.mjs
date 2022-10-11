10d3b38d7a4668e4b6c5fef9d2b92e1303fe3224
Mirha Masala <mirha@hey.com>
2022-10-11 14:31:49 +0100

    Create script to import WP content

---
+++ src/lib/importWordPressContent.mjs
@@ -0,0 +1,147 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+import renderPostTemplate from './renderPostTemplate.mjs'
+
+const NEWSLETTER_CATEGORY_ID = 'paper-planes-packets'
+
+const importWordPressContent = async () => {
+  try {
+    const [categories, tags, content] = await Promise.all([
+      fetchContent('categories'),
+      fetchContent('tags/?per_page=100'),
+      fetchContent('posts/?per_page=100'),
+      // fetchContent('pages/?per_page=100'),
+    ])
+
+    const contentWithCustomProperties = extractProperties(
+      categories,
+      tags,
+      content
+    )
+    writeContent(contentWithCustomProperties)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const fetchContent = async (endpoint) => {
+  const url = `https://mirhamasala.com/wp-json/wp/v2/${endpoint}`
+  const response = await fetch(url)
+  const body = await response.json()
+
+  if (response.status !== 200) {
+    throw Error(body.message)
+  }
+
+  return body
+}
+
+const extractProperties = (categoriesData, tagsData, content) => {
+  return content.map((entry) => {
+    let {
+      categories,
+      content: { rendered: content },
+      date_gmt,
+      excerpt: { rendered: excerpt },
+      modified_gmt,
+      id,
+      slug,
+      status,
+      tags,
+      title: { rendered: title },
+      type,
+    } = entry
+
+    return {
+      categories: convertIdsToSlugs(categories, categoriesData),
+      content,
+      date_gmt,
+      excerpt,
+      modified_gmt,
+      id,
+      slug,
+      status,
+      tags: convertIdsToSlugs(tags, tagsData),
+      title,
+      type,
+    }
+  })
+}
+
+const convertIdsToSlugs = (ids, data) => {
+  return ids.map((id) => {
+    return data.find((data) => data.id === id).slug
+  })
+}
+
+const writeContent = (content) => {
+  content.forEach((entry) => {
+    if (
+      entry.id === 3142 ||
+      entry.id === 2695 ||
+      entry.id === 2443 ||
+      entry.id === 2118 ||
+      entry.id === 2297 // page
+    )
+      return
+
+    if (isPost(entry)) {
+      createDirectoryAndWriteToFile(
+        `src/pages/posts/${entry.slug}`,
+        'index.mdx',
+        entry
+      )
+    }
+
+    if (isLetter(entry)) {
+      createDirectoryAndWriteToFile(
+        `src/pages/letters/${entry.slug}`,
+        'index.mdx',
+        entry
+      )
+    }
+
+    if (isPage(entry)) {
+      createDirectoryAndWriteToFile('src/pages/', `${entry.slug}.mdx`, entry)
+    }
+  })
+}
+
+const isPost = (entry) =>
+  entry.type === 'post' && !entry.categories.includes(NEWSLETTER_CATEGORY_ID)
+
+const isLetter = (entry) =>
+  entry.type === 'post' && entry.categories.includes(NEWSLETTER_CATEGORY_ID)
+
+const isPage = (entry) => entry.type === 'page'
+
+const createDirectoryAndWriteToFile = (dirSlug, fileSlug, entry) => {
+  const directoryName = path.join(process.cwd(), dirSlug)
+  const fileName = path.join(dirSlug, fileSlug)
+
+  createDirectory(directoryName)
+  createFile(fileName, entry)
+}
+
+const createDirectory = (name) => {
+  try {
+    if (!fs.existsSync(name)) {
+      fs.mkdirSync(name)
+    }
+  } catch (err) {
+    console.error(err)
+    return
+  }
+}
+
+const createFile = (name, entry) => {
+  fs.writeFile(name, renderPostTemplate(entry), 'utf8', (err) => {
+    if (err) console.log(err)
+    else {
+      console.log('File written successfully')
+    }
+  })
+}
+
+importWordPressContent()
