38b331b849f16f7872e35a0aa560e26e9b6aea0b
Mirha Masala <mirha@hey.com>
2022-11-05 12:03:22 +0000

    Create Spots component

---
+++ src/components/Spots.tsx
@@ -0,0 +1,51 @@
+import { categories, categoryNames } from "@/interfaces/categories";
+import { type Spot } from "@/interfaces/spots";
+import { spots } from "@/data/amsterdam";
+
+export function Spots() {
+  let spotsByCategory: Spot[] = [];
+
+  function getSpotsByCategory(category: string) {
+    spotsByCategory = spots.filter((spot) => spot.category === category);
+  }
+
+  return (
+    <>
+      {categoryNames.map((category) => {
+        getSpotsByCategory(category);
+        return (
+          <>
+            {spotsByCategory.length > 0 && (
+              <section key={category}>
+                <h3 className="flex gap-2">
+                  <span className="mr-4">{categories[category].emoji}</span>
+                  <span>{categories[category].label}</span>
+                </h3>
+                <ul>
+                  {spotsByCategory.map((spot) => {
+                    return (
+                      <>
+                        {spot.description.length > 0 && (
+                          <li key={spot.name}>
+                            <h4>
+                              {spot.googleMapsUrl ? (
+                                <a href={spot.googleMapsUrl}>{spot.name}</a>
+                              ) : (
+                                spot.name
+                              )}
+                            </h4>
+                            <p>{spot.description}</p>
+                          </li>
+                        )}
+                      </>
+                    );
+                  })}
+                </ul>
+              </section>
+            )}
+          </>
+        );
+      })}
+    </>
+  );
+}
