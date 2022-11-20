250bcd9fa3df7ef060e561d6933d3d304ee0e583
Mirha Masala <mirha@hey.com>
2022-11-20 20:06:07 +0000

    Add recipe folded sweet eggs

---
+++ src/pages/recipes/index.jsx
@@ -0,0 +1,10 @@
+import { SimpleLayout } from "@/components/SimpleLayout";
+
+export default function RecipesIndex() {
+  return (
+    <SimpleLayout
+      title="Recipes"
+      intro="A collection of my favorite recipes."
+    ></SimpleLayout>
+  );
+}
