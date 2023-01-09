1793647d774d4efc30a6ce6825e23b295fe38f88
Mirha Masala <mirha@hey.com>
2023-01-09 17:27:29 +0000

    Add client queries

---
+++ src/graphql/queries.js
@@ -0,0 +1,23 @@
+import { request, gql } from "graphql-request";
+
+const GRAPHQL_ENDPOINT = "http://localhost:3000/api/graphql";
+
+export async function getCategories() {
+  const query = gql`
+    query {
+      categories {
+        emoji
+        label
+        slug
+        spots {
+          description
+          googleMapsUrl
+          name
+          slug
+        }
+      }
+    }
+  `;
+  const { categories } = await request(GRAPHQL_ENDPOINT, query);
+  return categories;
+}
