8360e510005844604f7adc9f8dad2b58de3a6739
Mirha Masala <mirha@hey.com>
2023-02-12 10:59:24 +0100

    Extract PostFooter into PostLayout

--- src/components/PostLayout.jsx
+++ src/components/PostLayout.jsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-import Link from "next/link";
+import NextLink from "next/link";
 
 import { Container } from "@/components/Container";
+import { Link } from "@/components/Link";
+import { PostFooter } from "@/components/PostFooter";
 import { formatDate } from "@/lib/formatDate";
 import { Prose } from "@/components/Prose";
 
@@ -38,13 +40,13 @@ export function PostLayout({
         <div className="xl:relative">
           <div className="mx-auto max-w-2xl">
             {previousPathname && (
-              <Link
+              <NextLink
                 href={previousPathname}
                 aria-label="Go back to posts"
                 className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
               >
                 <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-              </Link>
+              </NextLink>
             )}
             <article>
               <header className="flex flex-col">
@@ -59,7 +61,17 @@ export function PostLayout({
                   <span className="ml-3">{formatDate(meta.date)}</span>
                 </time>
               </header>
-              <Prose className="mt-8">{children}</Prose>
+              <Prose className="mt-8">
+                {children}
+                {meta.related_reading && (
+                  <PostFooter>
+                    {meta.related_reading.emoji}{" "}
+                    <Link href={meta.related_reading.url}>
+                      {meta.related_reading.title}
+                    </Link>
+                  </PostFooter>
+                )}
+              </Prose>
             </article>
           </div>
         </div>
