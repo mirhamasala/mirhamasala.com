8df5b850abf3d3742abfece6cf35c5df9854e2ba
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2023-02-14 16:10:18 +0100

    Add amsterdam spots (#39)
    * Add graphql-request
    
    * Add client queries
    
    * Add Spots component
    
    * Show Amsterdam spots
    
    * Filter categories with spots in the resolver
    
    * Install Codegen + dependencies
    
    * Add codegen config
    
    * Move schema
    
    * Generate types for models and resolvers with codegen
    
    * Use env vars
    
    * Create useCategories
    
    * Add res, req types
    
    * CR
    
    * CR

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,7 +1,10 @@
 import { createSchema, createYoga } from "graphql-yoga";
+import { Resolvers } from "@/graphql/documents";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 import { categories } from "@/data/categories";
 import { spots } from "@/data/spots/amsterdam";
+import typeDefs from "schema.graphql";
 
 export const config = {
   api: {
@@ -9,55 +12,32 @@ export const config = {
   },
 };
 
-const typeDefs = `
-  type Category {
-    emoji: String!
-    label: String!
-    slug: String!
-    spots: [Spot!]!
-  }
-
-  type Geo {
-    latitude: Float!
-    longitude: Float!
-  }
-
-  type Spot {
-    category: Category!
-    description: String!
-    geo: Geo!
-    googleMapsUrl: String!
-    hasMarkdown: Boolean!
-    name: String!
-    published: Boolean!
-    slug: String!
-    url: String!
-  }
-
-  type Query {
-    categories: [Category!]!
-    spots: [Spot!]!
-  }
-`;
-
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
-    categories: () => categories,
-    spots: () => spots,
-  },
-  Category: {
-    spots: (category) =>
-      spots.filter((spot) => spot.category === category.slug && spot.published),
-  },
-  Spot: {
-    category: (spot) =>
-      categories.find((category) => category.slug === spot.category),
+    categories(_obj, { hasSpots }) {
+      if (!hasSpots) {
+        return categories;
+      }
+
+      return categories.reduce((categoriesWithSpots, category) => {
+        let categorySpots = spots.filter(
+          (spot) => spot.category === category.slug && spot.published
+        );
+
+        if (categorySpots.length) {
+          categoriesWithSpots.push({ ...category, spots: categorySpots });
+        }
+
+        return categoriesWithSpots;
+      }, []);
+    },
   },
 };
 
-const schema = createSchema({ typeDefs, resolvers });
-
-export default createYoga({
-  graphqlEndpoint: "/api/graphql",
-  schema,
+export default createYoga<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
+  schema: createSchema({ typeDefs, resolvers }),
+  graphqlEndpoint: process.env.NEXT_PUBLIC_API_URL,
 });
