09e473c1fd392a07046ed828c7e0f8878b167c35
Mirha Masala <mirha@hey.com>
2023-02-16 11:06:59 +0100

    CR

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,21 +1,36 @@
 import { useState, useEffect } from "react";
-import { getCategories } from "@/graphql/queries";
+
 import { type Category } from "@/graphql/documents";
+import { getCategories } from "@/graphql/queries";
+
+type StatusProps = "idle" | "pending" | "rejected" | "resolved";
 
 function useCategories() {
   const [categories, setCategories] = useState<Category[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [status, setStatus] = useState<StatusProps>("idle");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setLoading(true);
+    setStatus("pending");
 
-    getCategories().then(setCategories);
-
-    setLoading(false);
+    getCategories()
+      .then((data) => {
+        setCategories(data);
+        setStatus("resolved");
+      })
+      .catch((error) => {
+        setError(error.message);
+        setStatus("rejected");
+      });
   }, []);
 
-
-  return {categories, loading};
+  return {
+    categories,
+    error,
+    isLoading: status === "idle" || status === "pending",
+    isResolved: status === "resolved",
+    isRejected: status === "rejected",
+  };
 }
 
 export default useCategories;
