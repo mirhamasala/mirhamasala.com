87eb2505c7d8b8acae14b5cd3c4cb7f2f87c0062
Mirha Masala <mirha@hey.com>
2023-02-16 12:14:08 +0100

    CR

--- src/components/Spots.tsx
+++ src/components/Spots.tsx
@@ -13,28 +13,32 @@ export function Spots() {
       <div>
         {categories.map((category) => {
           return (
-            <section key={category.slug}>
-              <h3 className="flex gap-2">
-                <span className="mr-4">{category.emoji}</span>
-                <span>{category.label}</span>
-              </h3>
-              <ul>
-                {category.spots.map((spot) => {
-                  return (
-                    <li key={spot.slug}>
-                      <h4>
-                        {spot.googleMapsUrl ? (
-                          <a href={spot.googleMapsUrl}>{spot.name}</a>
-                        ) : (
-                          spot.name
-                        )}
-                      </h4>
-                      <p>{spot.description}</p>
-                    </li>
-                  );
-                })}
-              </ul>
-            </section>
+            <>
+              {category.spots.length > 0 && (
+                <section key={category.slug}>
+                  <h3 className="flex gap-2">
+                    <span className="mr-4">{category.emoji}</span>
+                    <span>{category.label}</span>
+                  </h3>
+                  <ul>
+                    {category.spots.map((spot) => {
+                      return (
+                        <li key={spot.slug}>
+                          <h4>
+                            {spot.googleMapsUrl ? (
+                              <a href={spot.googleMapsUrl}>{spot.name}</a>
+                            ) : (
+                              spot.name
+                            )}
+                          </h4>
+                          <p>{spot.description}</p>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </section>
+              )}
+            </>
           );
         })}
       </div>
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -13,17 +13,62 @@ export const config = {
   },
 };
 
+const emptySpots = [
+  {
+    category: "parks",
+    description:
+      "G and I only recently stumbled into these community gardens. They're cute. And just next to the Westerpark.",
+    geo: {
+      latitude: 52.38752,
+      longitude: 4.85745,
+    },
+    googleMapsUrl: "https://goo.gl/maps/A4kkrcMkuFKjRfzp7",
+    hasMarkdown: true,
+    name: "Tuinpark Nut & Genoegen",
+    published: true,
+    slug: "tuinpark-nut-en-genoegen",
+    url: "http://www.nutengenoegen.amsterdam/",
+  },
+  {
+    category: "parks",
+    description: "",
+    geo: {
+      latitude: 52.35799,
+      longitude: 4.86864,
+    },
+    googleMapsUrl: "https://www.google.com/maps?cid=7842025157712464437",
+    hasMarkdown: false,
+    name: "Vondelpark",
+    published: false,
+    slug: "vondelpark",
+    url: "https://www.hetvondelpark.net/",
+  },
+];
+
 const resolvers: Resolvers = {
   Query: {
-    categories: () => categories,
+    categories: () => {
+      return categories.map((category) => {
+        return {
+          ...category,
+          spots: [],
+        };
+      });
+    },
   },
   Category: {
-    spots: (category) =>
-      spots.filter((spot) => spot.category === category.slug),
-  },
-  Spot: {
-    category: (spot) =>
-      categories.find((category) => category.slug === spot.category.slug),
+    spots: (category) => {
+      const categoriesWithSpots = emptySpots.filter(
+        (spot) => spot.category === category.slug
+      );
+
+      return categoriesWithSpots.map((spot) => {
+        return {
+          ...spot,
+          category: category,
+        };
+      });
+    },
   },
 };
 
