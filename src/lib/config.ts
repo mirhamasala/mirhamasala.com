9f2ef96617b74621157b9500033e5fab9d9f5825
Mirha Masala <mirha@hey.com>
2023-02-20 14:52:51 +0000

    Fix add spots

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 
-assert(process.env.NEXT_PUBLIC_API_URL);
+assert(process.env.NEXT_PUBLIC_API_PATH);
 
-const config = {api_path: process.env.NEXT_PUBLIC_API_URL};
+const config = { api_path: process.env.NEXT_PUBLIC_API_PATH };
 
 export default config;
