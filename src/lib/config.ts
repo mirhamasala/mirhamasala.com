b9c85d8b0b0272a366ba48baa9d4b5b77c51fec6
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2023-02-20 15:23:14 +0100

    Fix add spots (#44)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,6 @@ import assert from "assert";
 
 assert(process.env.NEXT_PUBLIC_API_URL);
 
-const config = {api_path: process.env.NEXT_PUBLIC_API_URL};
+const config = { api_path: "/api/graphql" };
 
 export default config;
