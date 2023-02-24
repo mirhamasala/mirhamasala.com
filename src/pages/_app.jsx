6069a15057fe67e7f5a91881cb28b17462da9ed6
Mirha Masala <mirha@hey.com>
2023-02-24 13:00:35 +0000

    Wrap app in QueryClientProvider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MDXProvider } from "@mdx-js/react";
 import { useEffect, useRef } from "react";
 
@@ -19,24 +20,28 @@ function usePrevious(value) {
 }
 
 export default function App({ Component, pageProps, router }) {
+  const queryClient = new QueryClient();
   const components = {
     a: Link,
   };
   let previousPathname = usePrevious(router.pathname);
+
   return (
-    <MDXProvider components={components}>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+    <QueryClientProvider client={queryClient}>
+      <MDXProvider components={components}>
+        <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+          </div>
         </div>
-      </div>
-      <div className="relative">
-        <Header />
-        <main>
-          <Component previousPathname={previousPathname} {...pageProps} />
-        </main>
-        <Footer />
-      </div>
-    </MDXProvider>
+        <div className="relative">
+          <Header />
+          <main>
+            <Component previousPathname={previousPathname} {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+      </MDXProvider>
+    </QueryClientProvider>
   );
 }
