eca7fcf1980b8eb06327add3a6c1e4b4b0419d9f
Mirha Masala <mirha@hey.com>
2023-03-14 14:34:44 +0000

    Refactor

--- src/hooks/useCity.ts
+++ src/hooks/useCity.ts
@@ -2,24 +2,14 @@ import { request } from "graphql-request";
 import { useQuery } from "@tanstack/react-query";
 import { GetCityQuery, GetCityDocument } from "@/graphql/documents";
 
-function useCity(id: string) {
-  const {
-    data: { city } = { city: { spots: [] } },
-    isLoading,
-    isError,
-    error,
-  } = useQuery<GetCityQuery, Error>({
+const fetchCity = async (id: string) =>
+  await request("/api/graphql", GetCityDocument, { id });
+
+const useCity = (id: string) =>
+  useQuery<GetCityQuery, Error>({
     queryKey: ["city"],
-    queryFn: async () => await request("/api/graphql", GetCityDocument, { id }),
+    queryFn: () => fetchCity(id),
     enabled: !!id,
   });
 
-  return {
-    spots: city.spots,
-    isLoading,
-    isError,
-    error,
-  };
-}
-
-export default useCity;
+export { useCity, fetchCity };
