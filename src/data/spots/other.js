37471e7a38864897b3dfa478721637f3a04a65bf
Mirha Masala <mirha@hey.com>
2023-04-21 08:52:03 +0100

    Add other spots data

---
+++ src/data/spots/other.js
@@ -0,0 +1,47 @@
+export const other = [
+  {
+    category: "eats",
+    city: "tel-aviv",
+    description: "",
+    geo: {
+      latitude: 32.06462102488814,
+      longitude: 34.77264306969832,
+    },
+    googleMapsUrl: "https://goo.gl/maps/Em2jdPB67h7StQZG9",
+    hasMarkdown: false,
+    name: "Santa Katarina",
+    published: false,
+    slug: "santa-katarina",
+    url: "",
+  },
+  {
+    category: "sweets",
+    city: "melbourne",
+    description: "",
+    geo: {
+      latitude: -37.795118104496105,
+      longitude: 144.9817552010349,
+    },
+    googleMapsUrl: "https://goo.gl/maps/vi9QEFvHLSicFawi7",
+    hasMarkdown: false,
+    name: "Lune Croissanterie Fitzroy",
+    published: false,
+    slug: "lune-croissanterie-fitzroy",
+    url: "https://www.lunecroissanterie.com/",
+  },
+  {
+    category: "eats",
+    city: "barcelona",
+    description: "",
+    geo: {
+      latitude: 41.41550592114803,
+      longitude: 2.1440488706584953,
+    },
+    googleMapsUrl: "https://goo.gl/maps/TdYPnSE5QSprTVzS9",
+    hasMarkdown: false,
+    name: "Aranda's Grill",
+    published: false,
+    slug: "arandas-grill",
+    url: "http://www.asadordearanda.com/",
+  },
+];
