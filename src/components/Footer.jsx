3436946bbbb62ddded0423f15a31778e3a0c19d4
Mirha Masala <mirha@hey.com>
2023-07-12 12:55:52 +0100

    Fix color contrast footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ export function Footer() {
                 <NavLink href="/posts">Posts</NavLink>
                 <NavLink href="/letters">Letters</NavLink>
               </div>
-              <p className="text-sm text-zinc-400 dark:text-zinc-500">
+              <p className="text-sm text-zinc-500 dark:text-zinc-400">
                 &copy; {new Date().getFullYear()} Mirha Masala. All rights
                 reserved.
               </p>
