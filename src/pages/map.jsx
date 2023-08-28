df98538a4686a0b537d86ad6d2660f14354e7c35
Mirha Masala <mirha@hey.com>
2023-08-28 07:40:50 +0100

    Show loading state while saved city is being retrieved

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { Listbox } from "@headlessui/react";
 
-import { cities } from "@/data/cities";
+import { citiesThatCanBeSelected as cities } from "@/data/cities";
 import { getAllSpots } from "@/lib/getAllSpots";
 import { getPublishedSpotsWithCategoryAndCity } from "@/lib/getPublishedSpotsWithCategoryAndCity";
 
 import Map from "@/components/Map";
+import { Spinner } from "@/components/Spinner";
 import { SimpleLayout } from "@/components/SimpleLayout";
 
 function Icon(props) {
   return (
     <svg
+      className="h-5 w-5"
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="1.5"
-      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
       {...props}
     >
       {props.children}
@@ -26,31 +30,21 @@ function Icon(props) {
 
 function CheckIcon() {
   return (
-    <Icon className="h-5 w-5" aria-hidden="true">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M4.5 12.75l6 6 9-13.5"
-      />
+    <Icon>
+      <path d="M4.5 12.75l6 6 9-13.5" />
     </Icon>
   );
 }
 
 function ChevronUpDownIcon() {
   return (
-    <Icon className="h-5 w-5" aria-hidden="true">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-      />
+    <Icon>
+      <path d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
     </Icon>
   );
 }
 
 function CitySelect({ selectedCity, setSelectedCity }) {
-  const citiesThatCanBeSelected = cities.filter((city) => city.canBeSelected);
-
   const handleCityChange = (city) => {
     setSelectedCity(city);
     sessionStorage.setItem("selectedCity", city.id);
@@ -60,13 +54,15 @@ function CitySelect({ selectedCity, setSelectedCity }) {
     <div className="relative z-10 mb-12 w-full xs:w-72">
       <Listbox value={selectedCity} onChange={handleCityChange}>
         <Listbox.Button className="relative w-full cursor-default rounded-md border border-zinc-900/10 bg-white py-2 pl-3 pr-10 text-left shadow-md shadow-zinc-800/5 dark:border-zinc-700 dark:bg-zinc-800-50 dark:font-medium dark:text-zinc-200 sm:text-sm">
-          <span className="block truncate">{selectedCity.label}</span>
+          <span className="block truncate">
+            {selectedCity ? <Spinner /> : <Spinner />}
+          </span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <ChevronUpDownIcon />
           </span>
         </Listbox.Button>
         <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md border border-zinc-900/10 bg-white p-2 shadow-lg shadow-zinc-800/5 dark:border-zinc-700 dark:bg-zinc-800-50 dark:text-zinc-200 sm:text-sm">
-          {citiesThatCanBeSelected.map((city) => (
+          {cities.map((city) => (
             <Listbox.Option
               key={city.id}
               className={({ active }) =>
@@ -106,7 +102,7 @@ function MapPage({ spots }) {
   const publishedSpotsWithCategoryAndCity =
     getPublishedSpotsWithCategoryAndCity(spots);
 
-  const [selectedCity, setSelectedCity] = useState(cities[0]);
+  const [selectedCity, setSelectedCity] = useState(null);
 
   useEffect(() => {
     const savedCityId = sessionStorage.getItem("selectedCity");
@@ -141,10 +137,14 @@ function MapPage({ spots }) {
             selectedCity={selectedCity}
             setSelectedCity={setSelectedCity}
           />
-          <Map
-            center={selectedCity.geo}
-            spots={publishedSpotsWithCategoryAndCity}
-          />
+          {selectedCity ? (
+            <Map
+              center={selectedCity.geo}
+              spots={publishedSpotsWithCategoryAndCity}
+            />
+          ) : (
+            <Spinner />
+          )}
         </div>
       </SimpleLayout>
     </>
