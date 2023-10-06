e0e5b8dd430c7b96e34f3852bd85ce26a5d4fc37
Mirha Masala <mirha@hey.com>
2023-10-06 15:47:19 +0100

    Configure unlighthouse

---
+++ unlighthouse.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  site: "https://www.mirhamasala.com",
+  outputPath: "public/unlighthouse",
+  routerPrefix: "/unlighthouse",
+  cache: true,
+  debug: true,
+  scanner: {
+    device: "desktop",
+  },
+};
