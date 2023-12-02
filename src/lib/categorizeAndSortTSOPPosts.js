781d6d015f25c8135121d971d50cb011d9ec0d24
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2023-12-02 09:26:04 +0000

    Redesign TSOP homepage (#182)
    * Extract spinOffs data
    
    * Adapt copy and alt text images
    
    *
    
    This change addresses the need by:
    
    *
    
    * Refactor function categorizeAndSortTSOPPosts
    
    * Redesign and style TSOP homepage

--- src/lib/categorizeAndSortTSOPPosts.js
+++ src/lib/categorizeAndSortTSOPPosts.js
@@ -1,39 +1,27 @@
-const spinOffCategoriesOrdered = {
-  1: "polyglot",
-  2: "artist",
-  3: "artist",
-  4: "kiteboarder",
-  5: "philanthropist",
-  6: "dancer",
-  7: "entrepreneur",
-  8: "reader",
-  9: "snowboarder",
-  10: "surfer",
-  11: "traveler",
-  12: "photographer",
-};
+import spinOffs from "@/data/spinOffs.ts";
 
 export function categorizeAndSortTSOPPosts(posts) {
-  let categorizedPosts = posts.reduce((acc, post) => {
+  const categorizedPosts = {};
+
+  posts.forEach((post) => {
     post.categories.forEach((category) => {
-      if (Object.values(spinOffCategoriesOrdered).includes(category)) {
-        acc[category] = acc[category] || [];
-        acc[category].push(post);
+      if (spinOffs[category]) {
+        if (!categorizedPosts[category]) {
+          categorizedPosts[category] = [];
+        }
+        categorizedPosts[category].push(post);
       }
     });
-    return acc;
-  }, {});
-
-  return Object.entries(categorizedPosts).map(([categoryName, posts]) => {
-    posts.sort((a, b) => new Date(a.date) - new Date(b.date));
-    let spinoffNumber = Object.keys(spinOffCategoriesOrdered).find(
-      (key) => spinOffCategoriesOrdered[key] === categoryName
-    );
-
-    return {
-      spinoffNumber: spinoffNumber,
-      spinOffName: categoryName,
-      posts,
-    };
   });
+
+  return Object.keys(categorizedPosts)
+    .sort((a, b) => spinOffs[a].number - spinOffs[b].number)
+    .map((category) => ({
+      category,
+      number: spinOffs[category].number,
+      img: spinOffs[category].img,
+      posts: categorizedPosts[category].sort(
+        (a, b) => new Date(a.date) - new Date(b.date)
+      ),
+    }));
 }
--- src/pages/tsop/index.js
+++ src/pages/tsop/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import { Link } from "@/components/Link";
 import { SimpleLayout } from "@/components/SimpleLayout";
@@ -7,7 +8,7 @@ import { getAllContent } from "@/lib/getAllContent";
 import { categorizeAndSortTSOPPosts } from "@/lib/categorizeAndSortTSOPPosts";
 
 export default function postsIndex({ posts }) {
-  posts = categorizeAndSortTSOPPosts(posts);
+  let postsWithSpinOffDetails = categorizeAndSortTSOPPosts(posts);
 
   return (
     <>
@@ -22,32 +23,61 @@ export default function postsIndex({ posts }) {
         title="The Spin-Off Project"
         intro={
           <>
-            In 2012, I started a lifestyle experiment called The Spin-Off
-            Project. I picked twelve things I wanted to do or learn and devoted
-            a month to each. I wrote about my experiences on the now defunct{" "}
-            <Link
+            In 2012, I kicked off The Spin-Off Project, a personal lifestyle
+            experiment, where I dove into twelve different pursuits for a month
+            each. I shared my experiences on the now-defunct{" "}
+            <a
               href="https://web.archive.org/web/20230130015454/http://www.thespinoffproject.com/"
               className="text-teal-500 transition hover:text-teal-600 dark:hover:text-teal-400"
             >
               thespinoffproject.com
+            </a>
+            , and you can read all my posts from that time below. To learn more
+            about the project, have a peek at{" "}
+            <Link
+              href="/tsop/about"
+              className="text-teal-500 transition hover:text-teal-600 dark:hover:text-teal-400"
+            >
+              About The Spin-Off Project
             </Link>
-            . Below is an archive of all the posts and pages I published.
+            .
           </>
         }
       >
-        <div className="flex flex-col gap-y-16 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-          {posts.map(({ spinoffNumber, spinOffName, posts }) => (
-            <section key={spinOffName} id={spinOffName}>
-              <h2 className="text-base font-semibold capitalize tracking-tight text-zinc-800 dark:text-zinc-100">
-                {spinOffName} · spin-off no. {spinoffNumber}
-              </h2>
-              <ul className="prose">
-                {posts.map((post) => (
-                  <li className="prose" key={post.slug}>
-                    <Link href={`/tsop/posts/${post.slug}`}>{post.title}</Link>
-                  </li>
-                ))}
-              </ul>
+        <div className="flex flex-col gap-y-20">
+          {postsWithSpinOffDetails.map(({ category, img, number, posts }) => (
+            <section
+              key={category}
+              id={category}
+              className="relative isolate flex flex-col gap-6 lg:flex-row lg:gap-7"
+            >
+              <div className="relative aspect-[16/9] h-fit max-w-xl lg:aspect-square lg:w-60 lg:shrink-0">
+                <Image
+                  src={img.src}
+                  priority
+                  width={360}
+                  height={240}
+                  alt={img.alt}
+                  className="absolute inset-0 h-full w-full rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+                />
+              </div>
+              <div className="ml-1 max-w-lg sm:ml-2 lg:mt-1 lg:ml-0">
+                <span className="text-sm text-zinc-600 dark:text-zinc-400">
+                  Spin-Off No. {number}
+                </span>
+                <h2 className="mt-3 text-xl font-semibold capitalize leading-6 text-zinc-800 dark:text-zinc-100">
+                  {category}
+                </h2>
+                <ul className="mt-3 flex list-disc flex-col gap-2">
+                  {posts.map((post) => (
+                    <li className="prose ml-5 text-base" key={post.slug}>
+                      <Link href={`/tsop/posts/${post.slug}`}>
+                        {post.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </section>
           ))}
         </div>
