8c173f03f7032ec4cff8be24b2153ced39b11a0e
Mirha Masala <mirha@hey.com>
2023-12-02 09:21:19 +0000

    Extract spinOffs data

---
+++ src/data/spinOffs.ts
@@ -0,0 +1,88 @@
+const spinOffs = {
+  polyglot: {
+    number: 1,
+    img: {
+      src: "/pages/tsop/posts/polyglot-month/sitting-table_1b.jpg",
+      alt: "Mirha Masala seated at a table, gazing at a wall with the phrase 'How are you' written in four languages: Bosnian, Dutch, English, and Spanish.",
+    },
+  },
+  artist: {
+    number: 2,
+    img: {
+      src: "/pages/tsop/posts/artist-month/memory-bytes_2b.jpg",
+      alt: "Mirha Masala on the street picking up fallen love letters and putting them back into a trash bag.",
+    },
+  },
+  kiteboarder: {
+    number: 3,
+    img: {
+      src: "/pages/tsop/posts/kiteboarder-month/kiteboarding-cumbuco-brazil.jpg",
+      alt: "Mirha Masala standing on a beach in Cumbuco, Brazil, holding a kiteboard with a kite in the background.",
+    },
+  },
+  philanthropist: {
+    number: 4,
+    img: {
+      src: "/pages/tsop/posts/philanthropist-month/laptops-globe-table_1b.png",
+      alt: "Mirha Masala and her friend seated across from each other, each working on a laptop.",
+    },
+  },
+  dancer: {
+    number: 5,
+    img: {
+      src: "/pages/tsop/posts/dancer-month/standing-mirror-dance-school_1.jpg",
+      alt: "Mirha Masala standing inside a dance school.",
+    },
+  },
+  entrepreneur: {
+    number: 6,
+    img: {
+      src: "/pages/tsop/posts/entrepreneur-month/woman-suit-phone_1.jpg",
+      alt: "Mirha Masala seated on the floor making a phone call, with crumpled papers and her blazer scattered beside her.",
+    },
+  },
+  reader: {
+    number: 7,
+    img: {
+      src: "/pages/tsop/posts/reader-month/women-kindle-couch_1.jpg",
+      alt: "Digitally altered image showing two identical young women reading a Kindle: one seated on a couch, the other sitting on the floor in front of the couch.",
+    },
+  },
+  snowboarder: {
+    number: 8,
+    img: {
+      src: "/pages/tsop/posts/snowboarder-month/mirha-lying-down-snowboard_1-1.jpg",
+      alt: "Mirha Masala lying on the snow with her snowboard still attached to her feet.",
+    },
+  },
+  traveler: {
+    number: 9,
+    img: {
+      src: "/pages/tsop/posts/30-days-traveling/mirha-masala-georgetown_1.jpg",
+      alt: "Mirha Masala sitting on the back of a bike that is part of a street artwork in George Town, Malaysia, depicting two children riding the bike.",
+    },
+  },
+  surfer: {
+    number: 10,
+    img: {
+      src: "/pages/tsop/posts/surfer-month/mirha-walking-surfboard-blue_1.jpg",
+      alt: "Mirha Masala walking on a beach in Indonesia, carrying a surfboard.",
+    },
+  },
+  meditator: {
+    number: 11,
+    img: {
+      src: "/pages/tsop/posts/meditator-month/colette-miller-wings.jpg",
+      alt: "Artwork featuring the iconic wings by Colette Miller.",
+    },
+  },
+  photographer: {
+    number: 12,
+    img: {
+      src: "/pages/tsop/posts/photographer-month/mirha-camera-daylight_1.jpg",
+      alt: "Mirha Masala holding a camera in front of her face, taking a photo.",
+    },
+  },
+};
+
+export default spinOffs;
